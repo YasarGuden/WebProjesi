4446fc9d71856786b251dc80f9f8de077103c9ac
YasarGuden <yasarguden09@gmail.com>
2019-08-27 17:36:05 +0300

    WebProject

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|tr/ do
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :tweeets
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "tweeets#index"
+end
+end
